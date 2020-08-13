f53f10df74231f8d7bdaf927bb65457ee78eae4c
Nick Ford <nickforddesign@gmail.com>
2020-08-13 16:28:00 -0400

    update home copy

--- src/js/views/Home/index.vue
+++ src/js/views/Home/index.vue
@@ -12,6 +12,7 @@ const message = `I'm a software engineer
 from Boston, MA.
  
 i ❤ working with:
+typescript,
 javascript,
 node.js,
 vue,
@@ -19,12 +20,13 @@ scss,
 webpack,
 docker,
 linux,
-etc
+ruby/rails,
+and many other tools
  
 try typing "snakes", "tenprint", or "minesweeper"
-to see some demos...
+to see some fun stuff...
  
-<router-link to="/demo">or click here to see some skills</router-link>
+<router-link to="/demo">or click here to see a demo</router-link>
 
 `
 
